1381c2dad9c6223e0535562ac953b08b5b70a735
Jeffrey <jrondeau@degdigital.com>
2017-02-28 14:32:04 -0600

    Fix bug with running context index rules

---
+++ src/Elision.Feature.Library.Search/App_Start/RegisterDependencies.cs
@@ -0,0 +1,14 @@
+﻿using Elision.Foundation.Rules;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Elision.Feature.Library.Search
+{
+    public class RegisterDependencies : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IRulesRunner, RulesRunner>();
+        }
+    }
+}
